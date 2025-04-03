a5e509929934a88fa2f5673f6a3916d568e4b632
mamba-69 <jennypankajpatel0511@gmail.com>
2025-04-03 20:06:30 +0530

    Add TypeScript declarations for react-icons

---
+++ src/types/react-icons.d.ts
@@ -0,0 +1,9 @@
+declare module "react-icons/fi";
+declare module "react-icons/fa";
+declare module "react-icons/md";
+declare module "react-icons/ri";
+declare module "react-icons/bi";
+declare module "react-icons/ai";
+declare module "react-icons/bs";
+declare module "react-icons/hi";
+declare module "react-icons/*";
